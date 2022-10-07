5c3e91e4e2d8a96e1f378ed748f40ea454ccc047
Nshuti Elise <90415582+nshuticode@users.noreply.github.com>
2022-10-07 19:14:45 +0000

    added some comments

---
+++ main.py
@@ -0,0 +1,18 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+# string substracting and type conversion
+digit_1 = int(two_digit_number[0])
+digit_2 = int(two_digit_number[1])
+
+# adding two digits substracted and converted from string to integers
+result = digit_1 + digit_2
+print(result)
+
+
+
+
+
